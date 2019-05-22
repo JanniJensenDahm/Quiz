3066f1b0fb73b223daca6a611a284468e2216ef7
JanniJensenDahm <you@example.com>
2019-05-22 14:11:58 +0200

    update added

---
+++ src/app/update-quiz/update-quiz.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { TempDataService } from '../service/temp-data.service';
+import { ActivatedRoute } from '@angular/router';
+import { QuizActions } from '../quiz.actions';
+import { NgRedux } from '@angular-redux/store';
+import { AppState } from '../store';
+import { Quiz } from '../entities/quiz';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+
+@Component({
+  selector: 'app-update-quiz',
+  templateUrl: './update-quiz.component.html',
+  styleUrls: ['./update-quiz.component.scss']
+})
+export class UpdateQuizComponent implements OnInit {
+
+  quiz: Quiz;
+  quizzes: Quiz[];
+  editQuiz: FormGroup;
+
+  constructor(
+    private tempData: TempDataService,
+    private route: ActivatedRoute,
+    private quizActions: QuizActions,
+    private ngRedux: NgRedux<AppState>,
+    private activatedRoute: ActivatedRoute,
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.quizActions.getQuizzes();
+    this.ngRedux.select(x => x.quizzes).subscribe(result => {
+      this.quizzes = result.quizzes;
+    });
+
+    this.quiz = this.quizzes.find(quiz => quiz._id === this.activatedRoute.snapshot.params.id);
+    console.log(this.quiz)
+    this.editQuiz = this.fb.group({
+      title: [this.quiz.title, Validators.required],
+      questions: this.fb.array([])
+    })
+
+    this.quiz.questions.forEach(element => {
+      let index = 0;
+      const questions = this.editQuiz.controls.questions as FormArray;
+      questions.push(this.fb.group({
+        title: [element.title],
+        options: this.fb.array([])
+      }));
+      console.log(this.editQuiz)
+      //const options = (<FormGroup>questions.controls[index]).controls.options as FormArray;
+      const options = questions.controls[index].get('options') as FormArray;
+      console.log("options " + options)
+      options.push(this.fb.group({
+        answer: [''],
+        correct: [''] 
+      }))
+      index++;
+    })
+    console.log(this.editQuiz)
+  }
+}
